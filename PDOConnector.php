10f49eee1cb1cca3fc01557dcd04aabef9352803
Eugene UST <yliujz@connect.ust.hk>
2025-09-04 20:35:30 +0800

    change at 2035 20250904

--- PDOConnector.php
+++ PDOConnector.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=user', 'admin', 'pass');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=CV', 'ADMIN', 'PASS');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
--- index.php
+++ index.php
@@ -4,3 +4,11 @@ session_start();
 
 ?>
 
+<h1> Welcome to Curriculum Vitae Management System </h1>
+<h2> Please login your account </h2>
+<form>
+    <label for="email"> Email: </label>
+    <input type=text, name="email", id="email"> <br>
+    <input type=submit name="Login">
+</form>
+
